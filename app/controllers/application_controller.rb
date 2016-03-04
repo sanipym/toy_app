62cf3ca2047282de870eb94ead23bf76b080584a
sanipym <sanipym@gmail.com>
2016-03-04 10:48:18 +0000

    Add two tables user and micropost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render text:"welcome to here-----!"
-    
-  end
+ 
   
   
 end
